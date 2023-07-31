7be2a2c05301239eca70ab0ceb69b47f823a1906
lbare <levi.bare@gmail.com>
2023-07-31 12:34:01 -0700

    Disable scrolling

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,6 +1,27 @@
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
+const disableScrolling = (event: TouchEvent) => {
+  event.preventDefault();
+};
+
+const enableScrolling = () => {
+  document.removeEventListener("touchmove", disableScrolling, {
+    passive: false,
+  } as EventListenerOptions);
+};
+
 const Home = () => {
+  useEffect(() => {
+    document.addEventListener("touchmove", disableScrolling, {
+      passive: false,
+    });
+
+    return () => {
+      enableScrolling();
+    };
+  }, []);
+
   return (
     <div className="flex w-full h-screen justify-center items-center">
       <div className="flex h-1/4 flex-col justify-between items-center">
