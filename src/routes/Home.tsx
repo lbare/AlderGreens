5a14b29f3b21b72ddef7f7d261edea1425a961c8
lbare <levi.bare@gmail.com>
2023-07-31 10:39:01 -0700

    Setup routing

---
+++ src/routes/Home.tsx
@@ -0,0 +1,22 @@
+import viteLogo from "/vite.svg";
+
+const Home = () => {
+  return (
+    <>
+      <div>
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+};
+
+export default Home;
---
+++ src/routes/Scorecard.tsx
@@ -0,0 +1,14 @@
+import reactLogo from "../assets/react.svg";
+
+const Home = () => {
+  return (
+    <>
+      <div>
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </div>
+      <h1>Vite + React</h1>
+    </>
+  );
+};
+
+export default Home;
