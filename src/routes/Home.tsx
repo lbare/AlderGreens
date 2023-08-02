623aeaf8d81aa6bbde31ed704227520d62a51063
lbare <levi.bare@gmail.com>
2023-08-01 22:12:30 -0700

    Remove scroll disabling

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,18 +1,7 @@
-import { useEffect } from "react";
 import CustomButton from "../components/CustomButton";
 import HomeBG from "../assets/HomeBG.png";
 
-const disableScrolling = (event: TouchEvent) => {
-  event.preventDefault();
-};
-
 const Home = () => {
-  useEffect(() => {
-    document.addEventListener("touchmove", disableScrolling, {
-      passive: false,
-    });
-  }, []);
-
   return (
     <div
       className="flex w-full h-screen justify-center items-center"
@@ -22,7 +11,7 @@ const Home = () => {
       }}
     >
       <div className="flex w-3/4 h-1/4 flex-col justify-between items-center">
-        <CustomButton title="New Game" isTitle={true} page="players" />
+        <CustomButton title="New Game" isTitle={true} page="tracking" />
         <CustomButton title="Scores" isTitle={true} page="" />
       </div>
     </div>
