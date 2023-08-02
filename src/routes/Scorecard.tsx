3ab9af8ad54ba5d20881007d12a626e3311563d7
lbare <levi.bare@gmail.com>
2023-08-01 19:48:12 -0700

    Start scorecard

--- src/routes/Scorecard.tsx
+++ src/routes/Scorecard.tsx
@@ -1,14 +1,40 @@
-import reactLogo from "../assets/react.svg";
+const numPlayers = 4;
+const names = ["Blaine", "Colm", "Chris", "Ethan"];
 
-const Home = () => {
+const Scorecard = () => {
   return (
-    <>
-      <div>
-        <img src={reactLogo} className="logo react" alt="React logo" />
+    <div className="flex justify-evenly flex-col items-center h-screen">
+      <h1 className="font-archivo text-white text-4xl">Scoreboard</h1>
+      {/* Grid for top names */}
+      <div className="w-full">
+        <div className={`grid grid-cols-${numPlayers + 1} gap-1`}>
+          {/* Empty cell for the top-left corner */}
+          <div className="bg-blue-300 text-black font-bold text-center py-2"></div>
+          {/* Render player names in the top row */}
+          {names.map((name) => (
+            <div
+              key={name}
+              className="bg-blue-300 text-black font-bold text-center p-2"
+            >
+              {name}
+            </div>
+          ))}
+        </div>
+        {/* Grid for hole numbers and scores */}
+        <div className={`grid grid-cols-1 gap-2 mt-2 border`}>
+          {/* Render hole numbers in the left column */}
+          {Array.from({ length: 9 }, (_, index) => (
+            <div
+              key={index}
+              className="bg-red-300 text-black font-bold text-center p-2"
+            >
+              #{index + 1}
+            </div>
+          ))}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-    </>
+    </div>
   );
 };
 
-export default Home;
+export default Scorecard;
