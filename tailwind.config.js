5451d91840dbe1a271993d154c841b40554edfb0
lbare <levi.bare@gmail.com>
2023-08-01 19:14:30 -0700

    Add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,19 @@ export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}", "./src/*.{ts,tsx}"],
   theme: {
     extend: {
+      colors: {
+        green: {
+          100: "#ecf6ee",
+          200: "#c5e4cd",
+          300: "#9fd2ab",
+          400: "#78c089",
+          500: "#51ae68",
+          600: "#3f8751",
+          700: "#2d603a",
+          800: "#1b3a23",
+          900: "#09130c",
+        },
+      },
       fontFamily: {
         roboto: ["Roboto", "sans-serif"],
         archivo: ["Archivo Black", "sans-serif"],
