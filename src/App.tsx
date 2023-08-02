7cdcf84c5d3cf4a3e7a4e9e1c6d9abbbda3ebc79
lbare <levi.bare@gmail.com>
2023-08-01 22:12:42 -0700

    Add tracking page

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,21 @@ import Home from "./routes/Home";
 import Scorecard from "./routes/Scorecard";
 import Players from "./routes/Players";
 import { Routes, Route } from "react-router-dom";
+import Tracking from "./routes/Tracking";
 
 function App() {
   return (
-    <div className="flex h-screen w-screen flex-col bg-green-500">
+    <div
+      className="flex h-screen w-screen max-w-2xl flex-col bg-green-500"
+      style={{
+        backgroundAttachment: "fixed",
+        overflow: "hidden",
+      }}
+    >
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/players" element={<Players />} />
+        <Route path="/tracking" element={<Tracking />} />
         <Route path="/scorecard" element={<Scorecard />} />
       </Routes>
     </div>
