aa49058a2e322901228bf79e15b1eb815e812be7
Levi <47727379+lbare@users.noreply.github.com>
2023-09-05 15:00:23 -0700

    Move user to select page if nothing selected

--- src/routes/Game.tsx
+++ src/routes/Game.tsx
@@ -1,12 +1,20 @@
 import Tracking from "./Tracking";
-import { useState } from "react";
+import { useState, useContext } from "react";
 import Scorecard from "./Scorecard";
 import BottomBar from "../components/BottomBar";
+import { ScoreContext } from "../contexts/ScoreContext";
+import { useNavigate } from "react-router-dom";
 
 const Game = () => {
   const [activePage, setActivePage] = useState<"scorecard" | "tracking">(
     "scorecard"
   );
+  const { players } = useContext(ScoreContext);
+  const navigate = useNavigate();
+
+  if (!players || players.length === 0) {
+    navigate("/selectPlayers");
+  }
 
   return (
     <div className="flex h-full w-full">
